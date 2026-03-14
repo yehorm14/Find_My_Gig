8e8510e0a0ad2b9bcac48458d52d170a334b58b5
Akif Baig <3002377b@student.gla.ac.uk>
2026-03-14 15:41:59 +0000

    Completed the signup process and registered models

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Musician, Band, Listing, Review
 
-# Register your models here.
+# Register our models here, models will be visible on the admin panel
+
+admin.site.register(Musician)
+admin.site.register(Band)
+admin.site.register(Listing)
+admin.site.register(Review)
