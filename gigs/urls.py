25be82fa400145c14b9af44f4cf9488180e5b3c4
Yehor Mykhailov <3032950M@student.gla.ac.uk>
2026-03-17 13:39:34 +0000

    Urls Updated for Dashboard

--- gigs/urls.py
+++ gigs/urls.py
@@ -8,29 +8,23 @@ app_name = 'gigs'
 
 urlpatterns = [
     # --- MAIN PAGES ---
-    # When the user goes to "127.0.0.1:8000/", send them to the 'home' view.
     path('', views.home, name='home'),
 
     # --- GIGS ROUTING ---
-    # When the user goes to "/gigs/", show them the search engine and list of all gigs.
     path('gigs/', views.gig_listings, name='gig_listings'),
-    
-    # The <int:gig_id> part is a variable! If the user goes to "/gigs/5/", 
-    # Django grabs the number 5 and passes it to the 'gig_detail' view.
     path('gigs/<int:gig_id>/', views.gig_detail, name='gig_detail'),
-    
-    # Form page for users to create a new gig
     path('gigs/create/', views.create_gig, name='create_gig'),
 
     # --- USER PROFILES ROUTING ---
-    # Shows the list of all musicians
     path('musicians/', views.musicians_list, name='musicians_list'),
-    
-    # Grabs the specific ID from the URL to show a single musician's profile
     path('musicians/<int:id>/', views.musician_profile, name='musician_profile'),
-    
-    # Grabs the specific ID from the URL to show a single band's profile
     path('bands/<int:id>/', views.band_profile, name='band_profile'),
+
+    # --- USER PORTAL  ---
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('my-applications/', views.my_applications, name='my_applications'),
+    path('my-listings/', views.my_listings, name='my_listings'),
+    path('my-profile/', views.my_profile, name='my_profile'),
 
     # -- AUTHENTICATION AND SIGNUPS --
     path('signup/', views.signup_choice, name='signup'),
@@ -44,7 +38,6 @@ urlpatterns = [
              template_name='gigs/login.html',
              redirect_authenticated_user=True,), 
         name='login'),
-
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     # -- PASSWORD RESETS --
