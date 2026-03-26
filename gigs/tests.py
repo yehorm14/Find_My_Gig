39d61b58414e91d0cc9d970c0511270207735d25
“Cruz <2850192f@student.gla.ac.uk>
2026-03-26 06:08:44 +0000

    Adjusted tests for updated models

--- gigs/tests.py
+++ gigs/tests.py
@@ -1,16 +1,16 @@
 import django.core.exceptions
 from django.test import TestCase
-from rango.models import *
 import population_script
 from datetime import date
+import gigs.models as models
 from django.core.validators import MinValueValidator, MaxValueValidator
 class MusicianTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.test_user = population_script.add_user("thierrycnf", "lol", "2850192F@student.gla.ac.uk", True)
         cls.test_musician = population_script.add_musician(cls.test_user, "Guitar",
-                                                       "Looking for a band that needs a guitar player", "",
-                                                       "Glasgow")
+                                                       "Looking for a band that needs a guitar player", 18,
+                                                       "", "Glasgow")
 
     def test_username(self):
         self.assertEqual("thierrycnf", self.test_musician.user.username)
@@ -56,7 +56,9 @@ class ListingTest(TestCase):
         cls.test_band = population_script.add_band(cls.test_user, "Gouging Fire", "Glasgow",
                                               "Looking for a guitar player", "", )
         cls.test_listing = population_script.add_listing(cls.test_band, "Guitar player needed!", "Guitar",
-                                                         date(2026, 3, 24), True)
+                                                         date(2026, 3, 24), True,
+                                                         "Looking for a full-time drummer that has 3 years of experience",
+                                                         "Glasgow")
 
     def test_title(self):
         self.assertEqual("Guitar player needed!", self.test_listing.title)
@@ -77,8 +79,7 @@ class ReviewTest(TestCase):
                                                    "lol", "2850192F@student.gla.ac.uk", True)
         cls.test_user2 = population_script.add_user("notchface",
                                                     "haha", "cruzfru123@outlook.com", False)
-        cls.test_review = population_script.add_review(cls.test_user1, cls.test_user2, 3,
-                                                       "He's alright, what you get is what you see")
+        cls.test_review = models.Review.objects.create(reviewer=cls.test_user1, reviewee=cls.test_user2, rating=3, comment="He's alright, what you get is what you see!")
 
     def test_reviewer(self):
         self.assertEqual(self.test_user1, self.test_review.reviewer)
@@ -90,12 +91,11 @@ class ReviewTest(TestCase):
         self.assertEqual(3, self.test_review.rating)
 
     def test_comment(self):
-        self.assertEqual("He's alright, what you get is what you see", self.test_review.comment)
+        self.assertEqual("He's alright, what you get is what you see!", self.test_review.comment)
 
     def validation_error(self):
         caught_error = False
         try:
-            test_review = population_script.add_review(self.test_user1, self.test_user2, 20,
-                                                       "He's alright, what you get is what you see")
+            models.Review.objects.create(reviewer=self.test_user1, reviewee=self.test_user2, rating=20, comment="Great!")
         except django.core.exceptions.ValidationError:
             self.assertTrue(caught_error)
