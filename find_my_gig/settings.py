e16963ce48578cec17709f7d8099ccbad205e210
Akif Baig <3002377b@student.gla.ac.uk>
2026-03-27 00:22:08 +0000

    Email Reset Functionality

--- find_my_gig/settings.py
+++ find_my_gig/settings.py
@@ -24,8 +24,13 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DEFAULT_FROM_EMAIL = "noreply@findmygig.local"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
 # Quick-start development settings - unsuitable for production
